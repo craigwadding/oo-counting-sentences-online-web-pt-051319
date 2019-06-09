574d724ba33ed16426a26b466e2d6b61d7f0aafe
craig wadding <craig.wadding@gmail.com>
2019-06-09 02:15:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.question?("?")
   end
 
   def exclamation?
-
+    self.exclamation?
   end
 
   def count_sentences
-
+    self.count_sentences
   end
 end
